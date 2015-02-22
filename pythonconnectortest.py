3c3629cf6062d5f40efb8380ba37b2003a7637d0
daniel <stabdan@gmail.com>
2015-02-22 17:27:32 -0600

    now checks sql table for existing date and skips if found

--- pythonconnectortest.py
+++ pythonconnectortest.py
@@ -20,8 +20,12 @@ def lets_do_it(ticker, start_date, end_date):
 	global cursor
 	cursor = cnx.cursor()
 	table_test(ticker + "_tbl")
+	print "Table check done"
+	print "Starting data retrieval"
 	dicthist = pull_hist_data(ticker, start_date, end_date)
+	print "Data retrieval done"
 	add_data_sql(dicthist, ticker)
+	print "All Systems Operational"
 
 def pull_hist_data(ticker, start_date, end_date):
 	return Share(ticker).get_historical(start_date, end_date) 
@@ -29,19 +33,29 @@ def pull_hist_data(ticker, start_date, end_date):
 def add_data_sql(dicthist, ticker):
 #Iterates over List of Dictionaries passed off from pull_hist_data fuction	
 #Places data specified into SQL database table.
+#In each iteration table is checked to see if date already exists and skips if it does.
 	for d in dicthist:
+		stmt = ("SELECT date_fld FROM " + ticker + "_tbl " + "WHERE date_fld = %s ")
+		cursor.execute(stmt, (d['Date'],))
+		result = cursor.fetchone()
+		if result:
+			print "Skipped"
+			next
+			
+		else:
+			add_day = ("INSERT INTO " + ticker + "_tbl" + """
+				   (date_fld, volume_fld, adjclose_fld) 
+				   VALUES (%s, %s, %s)""")
 
-		add_day = ("INSERT INTO " + ticker + "_tbl" + """
-			   (date_fld, volume_fld, adjclose_fld) 
-			   VALUES (%s, %s, %s)""")
-
-		data_day = (d['Date'], d["Volume"], d["Adj_Close"])
-		cursor.execute(add_day, data_day)
+			data_day = (d['Date'], d["Volume"], d["Adj_Close"])
+			cursor.execute(add_day, data_day)
+			print "Added " + str(d['Date'])
 
 	cnx.commit()
 	cnx.close()
 
 def table_test(table_name):
+#Checks database for existing table and creates it if it does not.
 
 	stmt = ("SHOW TABLES LIKE %s ")
 
@@ -57,6 +71,7 @@ def table_test(table_name):
 				volume_fld int, 
 				adjclose_fld float)""") 
 		cursor.execute(create_table)
+		print 'Table created'
 
 
-lets_do_it(raw_input("Enter Ticker: "), raw_input("Enter Start Date: "), raw_input("Enter End Date :"))
+lets_do_it(raw_input("Enter Ticker: "), raw_input("Enter Start Date: "), raw_input("Enter End Date : "))
