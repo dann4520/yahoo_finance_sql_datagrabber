29e64d22720d1e9bd3b7159379043884e025eeb6
daniel <stabdan@gmail.com>
2015-02-26 15:38:22 -0600

    added table_name prefix to fld names and added records counts

--- pythonconnectortest.py
+++ pythonconnectortest.py
@@ -34,26 +34,32 @@ def add_data_sql(dicthist, ticker):
 #Iterates over List of Dictionaries passed off from pull_hist_data fuction	
 #Places data specified into SQL database table.
 #In each iteration table is checked to see if date already exists and skips if it does.
+	table_name = ticker + "_tbl"
+	skipcount = 0
+	incount = 0
 	for d in dicthist:
-		stmt = ("SELECT date_fld FROM " + ticker + "_tbl " + "WHERE date_fld = %s ")
+		stmt = ("SELECT " + table_name + "date " + "FROM " + table_name + " WHERE " + table_name + "date " + "= %s ")
 		cursor.execute(stmt, (d['Date'],))
 		result = cursor.fetchone()
 		if result:
 			print "Skipped"
+			skipcount += 1
 			next
 			
 		else:
-			add_day = ("INSERT INTO " + ticker + "_tbl" + """
-				   (date_fld, volume_fld, adjclose_fld) 
-				   VALUES (%s, %s, %s)""")
+			add_day = ("INSERT INTO " + table_name + " (" +
+				   table_name + "date, " + table_name + "volume, " + table_name + "adjclose) " +
+				   "VALUES (%s, %s, %s)")
 
 			data_day = (d['Date'], d["Volume"], d["Adj_Close"])
 			cursor.execute(add_day, data_day)
 			print "Added " + str(d['Date'])
+			incount += 1
 
 	cnx.commit()
 	cnx.close()
-
+	print str(skipcount) + " records skipped"
+	print str(incount) + " records inserted"
 def table_test(table_name):
 #Checks database for existing table and creates it if it does not.
 
@@ -66,10 +72,9 @@ def table_test(table_name):
 		return True
 	else:
 		print 'Table does not exist'
-		create_table = ("CREATE TABLE " + table_name + """ 
-				(date_fld date, 
-				volume_fld int, 
-				adjclose_fld float)""") 
+		create_table = ("CREATE TABLE " + table_name + " (" + 
+				table_name + "date date, " + table_name + "volume int, " + table_name + "adjclose float)") 
+				
 		cursor.execute(create_table)
 		print 'Table created'
 
